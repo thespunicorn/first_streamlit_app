65ef2296abd4b5aa04e12dd444594c1881934f18
thespunicorn <122322777+thespunicorn@users.noreply.github.com>
2023-01-18 12:31:21 -0600

    Update streamlit_app.py
    converted a few lines of code to a function

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,12 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 
+# Function definitions
+def get_fruityadvice_data(this_fruit_choice):
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  return streamlit.dataframe(fruityvice_normalized)
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -30,9 +36,8 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
 
