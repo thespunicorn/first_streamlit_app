2838144bb6cc99ec6dafcfda1bd60f5c43c7cd78
thespunicorn <122322777+thespunicorn@users.noreply.github.com>
2023-01-18 12:41:48 -0600

    Update streamlit_app.py
    added button to show list of fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,11 @@ def get_fruityvice_data(this_fruit_choice):
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
 
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+       
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -44,15 +49,21 @@ except URLError as e:
 streamlit.stop()
 
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+# my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+# my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruit_load_list")
+# my_cur.execute("select * from fruit_load_list")
 # my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
+# my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit Load List contains:")
-streamlit.dataframe(my_data_rows)
+# streamlit.dataframe(my_data_rows)
 
+# Button to display the list of fruits
+if streamlit.button('Get Fruit Load List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
-streamlit.write('TThank you for adding ', add_my_fruit)
+streamlit.write('Thank you for adding ', add_my_fruit)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
