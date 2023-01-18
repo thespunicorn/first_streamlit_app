ad569144484682f60836bcc540a66874cd549f16
thespunicorn <122322777+thespunicorn@users.noreply.github.com>
2023-01-18 12:08:23 -0600

    Update streamlit_app.py
    put newest stuff to the bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,5 @@
 import streamlit
 import pandas
-import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruit_load_list")
-# my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-streamlit.header("The Fruit Load List contains:")
-streamlit.dataframe(my_data_rows)
 
 
 streamlit.title('My Parents New Healthy Diner')
@@ -40,3 +31,14 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from fruit_load_list")
+# my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
+streamlit.header("The Fruit Load List contains:")
+streamlit.dataframe(my_data_rows)
+
